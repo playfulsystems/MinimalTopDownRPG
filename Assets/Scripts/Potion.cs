0659478e26541611bdb0e35c0284a555828720e2
Jonah Warren <jonah@playfulsystems.com>
2021-02-10 17:23:47 -0500

    comments

--- Assets/Scripts/Potion.cs
+++ Assets/Scripts/Potion.cs
@@ -4,23 +4,15 @@ using UnityEngine;
 
 public class Potion : MonoBehaviour
 {
-	public GameObject player;       // to set: drag the player game object to the inspector (while potion is selected)
-	public Sprite newSprite;           // to set: drag the sprite from the project window to the inspector (while potion is selected)
+	public GameObject player;   // to set: drag the player game object to the inspector
+	public Sprite oldMan;	// to set: drag the sprite from the project window to the inspector
 
-	// Update is called once per frame
 	void Update()
 	{
-		// checks to see if the player bounds intersects with the potion
-		// here is the SpriteRenderer documentation:
-		// https://docs.unity3d.com/ScriptReference/SpriteRenderer.html
-
 		if (GetComponent<SpriteRenderer>().bounds.Intersects(player.GetComponent<SpriteRenderer>().bounds))
 		{
 			// change the sprite of the player by accessing its sprite controller
-			player.GetComponent<SpriteRenderer>().sprite = newSprite;
-
-			// destroys itself (since gameObject refers to the game object this script is attached to)
-			Destroy(gameObject);
+			player.GetComponent<SpriteRenderer>().sprite = oldMan;
 		}
 	}
 }
